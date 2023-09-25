748da0d6fb873e7ed7537a4d69b3076cf2fe2d20
zubair-shah <shahzubair269@gmail.com>
2023-09-25 13:02:07 +0500

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,115 @@
+/* alert("hello world!");
+ alert("hello world!"); 
+  */
+//   alert("hello world!"); 
+// console.log('zubair')
+
+// prompt("plz enter your name");
+
+// console.log(Math.random() * 100);
+// console.log(Math.floor(Math.random() * 100));
+
+// let , var , const 
+
+// let name = "zubair";
+// let name1 = 'zubair';
+// let name2 = '32'
+// let name3 = `shaikh`;
+// let funActivity = "Let's learn JavaScript";
+// let question = 'Do you want to learn JavaScript? "Yes!"';
+// console.log(funActivity);
+
+// let number = 34;
+// let first_Name = "John";
+// console.log(name)
+// console.log(number);
+
+
+// let PI = 3.142;
+//  console.log(PI);
+//  PI = 40;
+//  console.log(PI)
+
+
+// let language = "JavaScript";
+// let message = `Let's learn ${language}`;
+// console.log(message);
+
+// let str3 = "New \nline.";
+// let str4 = "I'm containing a backslash: \\!";
+// console.log(str3);
+// console.log(str4);
+
+// let number = 34;
+// let number1 = -34;
+// let number2 = 3.4;
+
+
+// let doYouHaveLaptop  = true;
+// console.log(doYouHaveLaptop);
+
+// let str1 = "JavaScript is fun!";
+// let str2 = "JavaScript is fun!";
+// console.log("These two strings are the same:", str1 === str2);
+
+// let str1 = Symbol("JavaScript is fun!");
+// let str2 = Symbol("JavaScript is fun!");
+
+// console.log("These two strings are the same:", str1 === str2);
+
+// let unassigned = undefined;
+
+// console.log(unassigned);
+
+let terribleThingToDo = null;
+let lastName;
+console.log("Same undefined:", lastName === terribleThingToDo);
+
+// basic data type in javascript
+
+let a = "zubair" //'zubair' or `zubair` string
+let b =  43 // 3.5 , -21  number datatype
+let booleanData =  true // false  boolean datatype
+let bigIntData =  232323043953n //bigInt datatype
+let unDefinedData = undefined // undefined datatype
+let nullData = null // null datatype
+let symbolData = Symbol('I am unique') // symbol datatype
+
+
+console.log("str", typeof nullData);
+// console.log("nr", typeof nr);
+// console.log("bigNr", typeof bigNr);
+// console.log("bool", typeof bool);
+// console.log("sym", typeof sym);
+// console.log("undef", typeof undef);
+// console.log("unknown", typeof unknown);
+
+
+// let nr1 = 2;
+// let nr2 = "2";
+// console.log(nr1 * nr2)
+
+// let nr1 = 2;
+let nr2 = Number("this");
+// let nr3 = String(2323);
+// console.log(nr2)
+
+// console.log(typeof nr3);
+
+// console.log(nr1 + nr2);
+
+
+let num = Boolean(0);
+let num1 = Boolean(null);
+let num2 = Boolean(undefined);
+let num3 = Boolean('');
+let num4 = Boolean(nr2);
+let num5 = Boolean('zubair');   //true
+
+console.log(num)
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+
