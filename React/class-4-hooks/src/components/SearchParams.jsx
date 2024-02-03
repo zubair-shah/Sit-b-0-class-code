575ab835db91c15608cd5d1bd61ffc01129601f8
zubair-shah <shahzubair269@gmail.com>
2024-02-03 19:20:46 +0500

    updated code

--- React/class-4-hooks/src/components/SearchParams.jsx
+++ React/class-4-hooks/src/components/SearchParams.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const Breeds = [];
 
@@ -6,10 +6,27 @@ function SearchParams() {
     const [location, setLocation] = useState('');
     const [animal, setAnimal] = useState('dog');
     const [breed, setBreed] = useState('');
-    // let location = ""
+    const [pets, setPets] = useState([])
+    // let location = "zuibar";
     console.log(location)
     console.log(animal)
     console.log(breed)
+    console.log(pets)
+
+
+
+    useEffect(() => {
+        requestPets();
+    }, [])
+
+    async function requestPets() {
+        const response = await fetch(`http://pets-v2.dev-apis.com/pets?animal=dog&location=us`)
+        const data = await response.json()
+        let actualData = data.pets;
+        setPets(actualData)
+        console.log('data', data.pets)
+    }
+
     return (
         <div className="search-params">
             <form >
@@ -41,9 +58,25 @@ function SearchParams() {
                     </select>
                 </label>
 
-
+                <h1>{location}</h1>
                 <button>submit</button>
             </form>
+            <div>
+                {
+                    pets.map((item, ind) => {
+                        console.log('item', item)
+                        return (
+                            <div>
+                                <h1>{item.name}</h1>
+                                <h2>{item.animal}</h2>
+                                <h3>{item.state}</h3>
+                            </div>
+                        )
+                    })
+
+
+                }
+            </div>
         </div>
     )
 }
